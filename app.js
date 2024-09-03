c80bb84632478811744818f9cd910674e2fd7b07
Aj-mathL <abhijitmajumder545@gmail.com>
2024-09-03 23:50:20 +0530

    Compelete Html and Css and JavaScript file to make proper web page

---
+++ app.js
@@ -0,0 +1,30 @@
+let tablinks = document.getElementsByClassName("tab-links"); 
+let tabcontents = document.getElementsByClassName("tab-contents"); 
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link"); 
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab"); 
+    }
+
+    event.currentTarget.classList.add("active-link"); 
+    document.getElementById(tabname).classList.add("active-tab"); 
+}
+
+let sidemenu = document.getElementById("sidemenu"); 
+
+function openmenu(){
+    sidemenu.style.right= "0"; 
+}
+function closemenu(){
+    sidemenu.style.right= "-200px"; 
+}
+
+
+
+
+// https://script.google.com/macros/s/AKfycbyVv9xqNvhxGMntTkMEdypMLfytpTjTZPN_bJiDI8pdNsc9eDqEoHfdXX7zIAZmAIX36w/exec
+//for google sheet file
+

3500feadc166e8d6649cc75521edd40e38aa7613
Aj-mathL <abhijitmajumder545@gmail.com>
2024-09-06 01:07:16 +0530

    Try to make game but game not working some bugs here I try to fix them and game show soon!

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-let tablinks = document.getElementsByClassName("tab-links"); 
-let tabcontents = document.getElementsByClassName("tab-contents"); 
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link"); 
+var typed = new Typed('#element', {
+    strings: ['Coder!', 'Mathematician!', 'Fontend Developer!', 'Python Developer!', 'C/C++ Developer!'],
+    typeSpeed: 50,
+});
+
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab"); 
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
     }
 
-    event.currentTarget.classList.add("active-link"); 
-    document.getElementById(tabname).classList.add("active-tab"); 
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
-let sidemenu = document.getElementById("sidemenu"); 
+let sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right= "0"; 
+function openmenu() {
+    sidemenu.style.right = "0";
 }
-function closemenu(){
-    sidemenu.style.right= "-200px"; 
+function closemenu() {
+    sidemenu.style.right = "-200px";
 }
 
 
+
+
+
+ 
 
 
 // https://script.google.com/macros/s/AKfycbyVv9xqNvhxGMntTkMEdypMLfytpTjTZPN_bJiDI8pdNsc9eDqEoHfdXX7zIAZmAIX36w/exec
